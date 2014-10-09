b8662c30643655d7f299978f26843fd4d03e23f9
Bóta Gábor <bota.gabor@gravityrd.com>
2014-10-10 01:51:08 +0200

    Registering and sending to phone tested&working

--- src/main/java/com/bg/phrobe/Application.java
+++ src/main/java/com/bg/phrobe/Application.java
@@ -4,20 +4,20 @@ import com.bg.phrobe.entities.User;
 import com.bg.phrobe.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-@EnableAutoConfiguration
 @ComponentScan
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
-
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-
-
         UserRepository repository = context.getBean(UserRepository.class);
 
         // save a couple of customers
